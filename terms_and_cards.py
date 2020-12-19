bfe680b994949eefc690b7fe68e82bb6414295c0
AlexeyMalafeev <amalafeev@yandex.ru>
2020-12-19 11:30:18 +0300

    sentence-splitting function

--- terms_and_cards.py
+++ terms_and_cards.py
@@ -1,11 +1,11 @@
-from collections import Counter
+# from collections import Counter
 from pprint import pprint
 
 
-import pandas as pd
 from pyate import combo_basic
 # noinspection PyPackageRequirements
 import RAKE
+import spacy
 from stop_words import get_stop_words
 
 ####################################################################################################
@@ -21,10 +21,18 @@ non_empty_lines = [parag for line in input_text.split('\n') if (parag := line.st
 stop_words = get_stop_words('en')
 rake_obj = RAKE.Rake(stop_words=stop_words)
 
+nlp = spacy.load('en_core_web_sm')
 
 ####################################################################################################
 # Functions
 ####################################################################################################
+
+
+def get_sentences(text: str) -> tuple:
+    """Split text into sentences with spaCy"""
+    doc = nlp(text)
+    return tuple(sent_stripped for sent in doc.sents if (sent_stripped := sent.text.strip()))
+
 
 def get_terms(text: str) -> tuple:
     """pyate is Python implementation of term extraction algorithms such as C-Value, Basic, Combo
@@ -38,5 +46,6 @@ def get_terms_rake(text: str) -> tuple:
     return tuple(word for word, score in rake_obj.run(text, maxWords=1, minFrequency=2))
 
 
-pprint(get_terms_rake(input_text)[:20])
-pprint(get_terms(input_text)[:20])
+# pprint(get_terms_rake(input_text)[:20])
+# pprint(get_terms(input_text)[:20])
+print(get_sentences(input_text))
