69c45d118b00ffc8e5c70349974ad27b0b4ad5ee
AlexeyMalafeev <amalafeev@yandex.ru>
2021-01-08 23:38:02 +0300

    fix bug in gap making

--- src/card_generation.py
+++ src/card_generation.py
@@ -1,8 +1,6 @@
 import re
 
-from setup import nlp
 import term_extraction
-import ui
 
 ####################################################################################################
 # Constants
@@ -38,7 +36,8 @@ def make_candidate_cards(snippet: str, prefix: str) -> tuple:
     return tuple(temp_cards)
 
 
-def make_gap(text: str, text_lower: str, target: str, gap: str = WORD_GAP) -> str:
+def make_gap(text: str, text_lower: str, target: str, gap: str = WORD_GAP) -> tuple:
+    orig_target = target
     try:
         matches = re.finditer(re.escape(target), text_lower)
     except re.error:
@@ -52,11 +51,18 @@ def make_gap(text: str, text_lower: str, target: str, gap: str = WORD_GAP) -> st
         text = text[:start] + gap + text[end:]
     try:
         gap_escaped = re.escape(gap)
-        text = re.sub(f'(^| )a ' + gap_escaped, fr'\1a(n) ' + gap_escaped, text)
-        text = re.sub(f'(^| )an ' + gap_escaped, fr'\1a(n) ' + gap_escaped, text)
-        text = re.sub(f'(^| )A ' + gap_escaped, fr'\1A(n) ' + gap_escaped, text)
-        text = re.sub(f'(^| )An ' + gap_escaped, fr'\1A(n) ' + gap_escaped, text)
+        text = re.sub(f'(^| )a ' + gap_escaped, fr'\1a(n) ' + gap, text)
+        text = re.sub(f'(^| )an ' + gap_escaped, fr'\1a(n) ' + gap, text)
+        text = re.sub(f'(^| )A ' + gap_escaped, fr'\1A(n) ' + gap, text)
+        text = re.sub(f'(^| )An ' + gap_escaped, fr'\1A(n) ' + gap, text)
     except re.error:
         print(f'{text = }, {target = }, {gap = }')
         raise
     return text, orig_target
+res = make_gap(
+    text='Scale out is a growth architecture',
+    text_lower='scale out is a growth architecture',
+    target='growth architecture',
+    gap=PHRASE_GAP,
+)
+print(res)
